6b1f9610b58253865dd5b66e4ff5cf96c220359f
1nv1s1ble <iieric@163.com>
2021-03-08 11:12:29 +0800

    Create 121. Best Time to Buy and Sell Stock.cpp

---
+++ Leetcode/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.empty())
+            return 0;
+        auto minPrices = prices.front();
+        auto maxProfit = 0;
+        for(const auto &i : prices)
+        {
+            maxProfit = max(maxProfit, i - minPrices);
+            minPrices = min(minPrices, i);
+        }
+        /*
+        auto begin = prices.cbegin();
+        auto end = prices.cend();
+        for(; begin != end; ++begin)
+        {
+            auto t = *begin;
+            maxProfit = max(maxProfit, t - minPrices);
+            minPrices = min(minPrices, t);
+        }
+        */
+        return maxProfit;
+    }
+};
